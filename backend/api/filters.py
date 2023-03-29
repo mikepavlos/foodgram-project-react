9c6ff7d6f2065a4678a6af776698764109dec414
mikepavlos <pavlovichmihaylovich@yandex.ru>
2023-03-29 12:41:46 +0300

    Local dockers - correct filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,15 @@
 from django_filters.rest_framework import FilterSet, filters
+from rest_framework.filters import SearchFilter
 
-from recipes.models import Recipe, Tag
+from recipes.models import Ingredient, Recipe, Tag
+
+
+class IngredientSearch(SearchFilter):
+    search_param = 'name'
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
 
 
 class RecipeFilter(FilterSet):
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet as DjoserUserViewSet
-from rest_framework import filters, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .filters import RecipeFilter
+from .filters import IngredientSearch, RecipeFilter
 from .pagination import Paginator
 from .permissions import IsAuthorOrAdminOrReadOnly
 from recipes.models import (
@@ -88,7 +88,7 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = (IngredientSearch,)
     search_fields = ('^name',)
 
 
